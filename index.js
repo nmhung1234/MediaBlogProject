ed5d3eaf14a145cbe93d5f62ddef8bcd124ce2cf
nmhung1234 <nmhung1234@hotmail.com>
2020-12-02 13:31:28 +0700

    up

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const middlewares = jsonServer.defaults()
 const PORT = process.env.PORT||3000
 server.use(middlewares)
 server.use(router)
+server.get('/', (req, res) => {
+    res.render('Dashboard.html')
+})
 server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
