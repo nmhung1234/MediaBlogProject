34bb585a31d7d8537d9c6f1228a804aebbbe3bc7
nmhung1234 <nmhung1234@hotmail.com>
2020-12-02 13:17:16 +0700

    up

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
- 
+const PORT = process.env.PORT||3000
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
